9e147ecb3e972b5746a3666f90b5fd69e0bcb545
Saw <ehkalanephaung96@gmail.com>
2023-10-23 22:21:40 +0700

    Update type interface

--- FrontEnd/src/type.ts
+++ FrontEnd/src/type.ts
@@ -1,19 +1,30 @@
-export interface StudentDetail {
-  id: number
-  FirstName: string
-  LastName: string
-  Student_ID: string
-  registered: string
-  advisor: number
-  stu_pic: string
+export interface StudentItem {
+  id: number;
+  name: string;
+  surname: string;
+  studentID: string;
+  image: string[];
+  department: string;
+  advisor: AdviserItem;
 }
 
-export interface AdvisorDetail {
-  id: number
-  FirstName: string
-  LastName: string
-  Department?: string
-  Email?: string
-  Office?: string
-  advisor_pic: string
+export interface AdviserItem {
+  id: number;
+  advisorID: string;
+  name: string;
+  surname: string;
+  position: string;
+  image: string[];
+  department: string;
+  studentList: StudentItem[];
+  student: StudentData;
+}
+
+export interface StudentData {
+  id: number;
+  name: string;
+  surname: string;
+  studentID: string;
+  image: string[];
+  department: string;
 }
