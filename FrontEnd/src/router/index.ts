8a8a8acf38b6477ee2bf5bdc164205922fe86c87
Saw <ehkalanephaung96@gmail.com>
2023-10-23 22:36:04 +0700

    Update router configuration

--- FrontEnd/src/router/index.ts
+++ FrontEnd/src/router/index.ts
@@ -1,124 +1,127 @@
-import { useStudentStore } from '@/stores/student'
-import { useTeacherStore } from '@/stores/teacher'
-import NProgress from 'nprogress'
-import 'nprogress/nprogress.css'
-import { createRouter, createWebHistory } from 'vue-router'
-import AdvisorView from '../views/AdvisorView.vue'
-import FormView from '../views/Form.vue'
-import Login from '../views/Login.vue'
-import Register from '../views/Register.vue'
-import StudentAdvisorView from '../views/StudentAdvisorView.vue'
-import StudentDetailView from '../views/StudentDetailView.vue'
-import StudentLayoutView from '../views/StudentLayoutView.vue'
-import StudentView from '../views/StudentView.vue'
+import StudentService from "@/services/StudentService";
+import { useStudentStore } from "@/stores/student";
+import AddAdviserView from "@/views/AddAdviserView.vue";
+import AdviserProfile from "@/views/AdviserProfile.vue";
+import AdviserView from "@/views/AdviserView.vue";
+import NetworkErrorView from "@/views/NetworkErrorView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
+import StudentView from "@/views/StudentView.vue";
+import AdviserDetail from "@/views/event/AdviserDetail.vue";
+import LayoutView from "@/views/event/LayoutView.vue";
+import StudentDetail from "@/views/event/StudentDetail.vue";
+import UpdateStudent from "@/views/event/updateStudent.vue";
+import UpdateAdviserView from "@/views/updateAdvisor.vue";
+import NProgress from "nprogress";
+import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'students',
-      component: StudentView
+      path: "/",
+      name: "students",
+      component: StudentView,
+      props: (route) => ({
+        page: parseInt((route.query?.page as string) || "1"),
+      }),
     },
     {
-      path: '/advisor',
-      name: 'advisor',
-      component: AdvisorView
+      path: "/advisors",
+      name: "professer",
+      component: AdviserView,
+      props: (route) => ({
+        page: parseInt((route.query?.page as string) || "1"),
+      }),
     },
     {
-      path: '/student/:id',
-      name: 'student-layout',
-      component: StudentLayoutView,
+      path: "/advisors/:id",
+      name: "update-advisor",
+      component: UpdateAdviserView,
+    },
+    {
+      path: "/AddView",
+      name: "AddView",
+      component: AddAdviserView,
+    },
+    {
+      path: "/student/:id",
+      name: "event-layout",
+      component: LayoutView,
       props: true,
+      beforeEnter: (to) => {
+        const id: number = parseInt(to.params.id as string);
+        const studentStore = useStudentStore();
+        return StudentService.getStudentById(id)
+          .then((response) => {
+            // need to set up the data
+            studentStore.setStudent(response.data);
+            StudentService.getAdviserById(Number(response.data.id)).then(
+              (response2) => {
+                studentStore.setAdviser(response2.data);
+              }
+            );
+          })
+          .catch((error) => {
+            if (error.response && error.response.status === 404) {
+              return {
+                name: "404-resource",
+                params: { resource: "student" },
+              };
+            } else {
+              return { name: "network-error" };
+            }
+          });
+      },
       children: [
         {
-          path: '',
-          name: 'student-detail',
-          component: StudentDetailView,
-          props: true
+          path: "/students/:id",
+          name: "student-detail",
+          component: StudentDetail,
+          props: true,
         },
         {
-          path: 'advisor/:advisorId',
-          name: 'student-advisor',
-          component: StudentAdvisorView,
-          props: true
+          path: "/student/adviser/:id",
+          name: "adviser-detail",
+          component: AdviserDetail,
+          props: true,
         },
         {
-          path: '/Login',
-          name: 'Login',
-          component: Login
+          path: "/student/:id",
+          name: "update-student",
+          component: UpdateStudent,
+          props: true,
         },
-        {
-          path: '/Register',
-          name: 'Register',
-          component: Register
-        },
-        {
-          path: '/From',
-          name: 'Form',
-          component: FormView,
-          props: true
-        }
-      ]
+      ],
     },
     {
-      path: '/Form',
-      name: 'Form',
-      component: FormView,
-      props: true
-    }
-  ]
-})
+      path: "/:catchAll(.*)",
+      name: "not-found",
+      component: NotFoundView,
+    },
+    {
+      path: "/404/:resource",
+      name: "404-resource",
+      component: NotFoundView,
+      props: true,
+    },
+    {
+      path: "/network-error",
+      name: "network-error",
+      component: NetworkErrorView,
+    },
+    {
+      path: "/professer/profile/:id",
+      name: "professer-profile",
+      component: AdviserProfile,
+      props: true,
+    },
+  ],
+});
 
-router.beforeEach(async () => {
-  NProgress.start()
-  const teacherStore = useTeacherStore()
-  const studentStore = useStudentStore()
-  if (teacherStore.teachers.length === 0) {
-    await teacherStore.fetchTeachersFromDB()
-  }
-  if (studentStore.students.length === 0) {
-    await studentStore.fetchStudentsFromDB()
-  }
-})
-
-// Start the progress bar on navigation
 router.beforeEach(() => {
-  NProgress.start()
-})
-
-// Complete the progress bar after navigation
+  NProgress.start();
+});
 router.afterEach(() => {
-  NProgress.done()
-})
+  NProgress.done();
+});
 
-// const fetchStudents = async () => {
-//   try {
-//     const response = await StudentService.getStudent();
-//     return response.data;
-//   } catch (error) {
-//     console.error('Error fetching students:', error);
-//     return [];
-//   }
-// };
-
-// router.beforeEach(async () => {
-//   const studentData: StudentDetail[] = await fetchStudents();
-//   StudentService.getStudent(studentData);
-// })
-
-// const fetchTeacher = async () => {
-//   try {
-//     const response = await TeacherService.getTeacher();
-//     return response.data;
-//   } catch (error) {
-//     console.error('Error fetching Teacher:', error);
-//     return [];
-//   }
-// };
-
-// router.beforeEach(async () => {
-//   const TeacherData: TeacherDetail[] = await fetchTeacher();
-//   StudentService.getStudent(TeacherData);
-// })
-
-export default router
+export default router;
