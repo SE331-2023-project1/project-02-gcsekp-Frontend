25c6bbd3a5d526f214dc6dbca24141e324f0dc1a
Saw <ehkalanephaung96@gmail.com>
2023-10-23 22:36:35 +0700

    Refactor App.vue

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -1,59 +1,50 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia'
-import { RouterLink, RouterView } from 'vue-router'
-import { useMessageStore } from './stores/message'
-
-const store = useMessageStore()
-const { message } = storeToRefs(store)
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <div class="m-0flex flex-col items-center justify-start bg-gray-800 w-full rounded-lg m-0">
-    <header>
-      <nav class="mt-6">
-        <ul class="flex border-b">
-          <li class="m-2">
-            <div
-              class="rounded-lg py-2 px-4 text-white hover:text-teal-600 font-mono text-lg font-bold"
-            >
-              <RouterLink to="/">Student</RouterLink>
-            </div>
-          </li>
-
-          <li class="m-2">
-            <div
-              class="rounded-lg py-2 px-4 text-white hover:text-teal-600 font-mono text-lg font-bold"
-            >
-              <RouterLink to="/advisor">Advisor</RouterLink>
-            </div>
-          </li>
-          <li class="m-2">
-            <div
-              class="rounded-lg py-2 px-4 text-white hover:text-teal-600 font-mono text-lg font-bold"
-            >
-              <RouterLink to="/Form"> Form </RouterLink>
-            </div>
-          </li>
-
-          <li class="m-2 ml-auto">
-            <div
-              class="rounded-lg py-2 px-4 text-white hover:text-teal-600 font-mono text-lg font-bold"
-            >
-              <RouterLink to="/Login">Login</RouterLink>
-            </div>
-          </li>
-        </ul>
-      </nav>
-
-      <!-- <img alt="Vue logo" class="logo" src="@/assets/management.png" width="125" height="125" />
-    <div class="wrapper">
-      <nav class="font-mono text-yellow-300" >
-        <RouterLink to="/">Student</RouterLink>
-        <RouterLink to="/advisor">Advisor</RouterLink>
-      </nav>
-    </div> -->
-    </header>
-  </div>
-
-  <RouterView />
+  <header>
+    <nav
+      class="flex justify-between items-center text-white font-mono mb-2 bg-red-800"
+    >
+      <div class="flex items-center font-mono">
+        <img
+          alt="Vue logo"
+          src="https://logolook.net/wp-content/uploads/2021/11/Hogwarts-Logo.png"
+          class="mt-5 mb-5 ml-20 h-20 w-70"
+        />
+        <p class="font-bold font-mono text-2xl">HOGWARTS MAGIC AWAKENED</p>
+      </div>
+      <div class="space-x-10 mr-20 font-mono">
+        <RouterLink
+          to="/"
+          class="font-black transition-colors duration-300 hover:text-yellow-500 text-lg"
+          active-class="active-link"
+          exact-active-class="active-link"
+          >Student</RouterLink
+        >
+        <RouterLink
+          to="/advisors"
+          class="font-black transition-colors duration-300 hover:text-yellow-500 text-lg"
+          active-class="active-link"
+          exact-active-class="active-link"
+          >Advisor</RouterLink
+        >
+        <RouterLink
+          to="/AddView"
+          class="font-black transition-colors duration-300 hover:text-yellow-500 text-lg"
+          active-class="active-link"
+          exact-active-class="active-link"
+          >Add Adviser</RouterLink
+        >
+      </div>
+    </nav>
+    <RouterView />
+  </header>
 </template>
+
+<style scoped>
+.active-link {
+  color: rgb(255, 223, 63);
+}
+</style>
