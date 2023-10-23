f81ff600cb057ca4dcac7a7a29f76db6765906e9
Saw <ehkalanephaung96@gmail.com>
2023-10-24 02:39:49 +0700

    update student card

--- FrontEnd/src/components/StudentCard.vue
+++ FrontEnd/src/components/StudentCard.vue
@@ -1,39 +1,34 @@
+<script setup lang="ts">
+import type { StudentItem } from '@/type'
+import { type PropType } from 'vue'
+const props = defineProps({
+  student: {
+    type: Object as PropType<StudentItem>,
+    require: true
+  }
+})
+</script>
 <template>
-  <RouterLink :to="{ name: 'student-detail', params: { id: student.id } }">
-    <div v-if="student" class="StudentDetail-class">
-      <div
-        class="StudentDetail-class border-2 w20-h20 rounded-lg shadow-lg mt-5 mb-5 w-250px bg-gray-100 pd-10 py-8 text-teal-600 text-base font-mono cursor-pointer flex flex-col justify-center items-center transition-transform transform-gpu hover:scale-105 hover:shadow-md"
-      >
-        <img class="mt-5 w-30 h-30 rounded-full" :src="student.stu_pic" alt="img" />
-        <span class="mt-5 font-bold text-xl">
-          {{ `${student.FirstName.toUpperCase()} ${student.LastName.toUpperCase()}` }}</span
-        >
+  <RouterLink :to="{ name: 'student-detail', params: { id: student?.id } }">
+    <div class="flex justify-center items-center mb-2">
+      <div v-if="student" class="border-2 w-3/4 h-38 p-2 bg-red-800 text-white rounded-md">
+        <div class="flex row-auto">
+          <div class="mr-2">
+            <img
+              v-for="image in student?.image"
+              :key="image"
+              :src="image"
+              alt=" student image"
+              class="border-2 border-black h-36 w-32 object-cover"
+            />
+          </div>
+          <div class="ml-2 text-left">
+            <p class="text-lg font-mono font-bold">Student ID: {{ student?.studentID }}</p>
+            <p>Name: {{ student?.name }}</p>
+            <p>Surname: {{ student?.surname }}</p>
+          </div>
+        </div>
       </div>
     </div>
   </RouterLink>
 </template>
-
-<script setup lang="ts">
-import { type StudentDetail } from '@/type'
-import { ref, type PropType } from 'vue'
-
-const StudentDetail = ref({})
-const props = defineProps({
-  student: {
-    type: Object as PropType<StudentDetail>,
-    required: true
-  }
-})
-</script>
-
-<!-- <style scoped>
-.StudentDetail-class {
-  width: 300px;
-  cursor: pointer;
-}
-  
-.StudentDetail-class:hover {
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
-}
-</style> -->
